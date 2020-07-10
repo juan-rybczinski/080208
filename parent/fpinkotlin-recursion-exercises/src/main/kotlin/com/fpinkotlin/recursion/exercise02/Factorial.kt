5f3153a8bd325dca756daf51ee2698c6270dcf1a
juan-rybczinski <jhreplay.lee@gmail.com>
2020-07-11 08:47:44 +0900

    연습문제 4-2: 재귀적 게승 함수값 작성

--- parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise02/Factorial.kt
+++ parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise02/Factorial.kt
@@ -1,5 +1,12 @@
 package com.fpinkotlin.recursion.exercise02
-
 object Factorial {
-    val factorial: (Int)-> Int = TODO("factorial")
+//    private lateinit var fact: (Int)-> Int
+//
+//    init {
+//        fact = { n -> if (n <= 1) n else n * fact(n - 1) }
+//    }
+//
+//    val factorial = fact
+    val factorial: (Int) -> Int by lazy { { n: Int -> if (n <= 1) n else n * factorial(n - 1) } }
 }
+
