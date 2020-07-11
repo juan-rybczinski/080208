0ad87059dd62bf4bca8d037b448bf8b9c520e936
juan-rybczinski <jhreplay.lee@gmail.com>
2020-07-11 11:23:30 +0900

    연습문제 4-10: 다형적 unfold 구현

--- parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise10/Unfold.kt
+++ parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise10/Unfold.kt
@@ -1,5 +1,13 @@
 package com.fpinkotlin.recursion.exercise10
 
 
-fun <T> unfold(seed: T, f: (T) -> T, p: (T) -> Boolean): List<T> = TODO("unfold")
+fun <T> unfold(seed: T, f: (T) -> T, p: (T) -> Boolean): List<T> {
+    val result = mutableListOf<T>()
+    var elem = seed
+    while (p(elem)) {
+        result.add(elem)
+        elem = f(elem)
+    }
+    return result
+}
 
