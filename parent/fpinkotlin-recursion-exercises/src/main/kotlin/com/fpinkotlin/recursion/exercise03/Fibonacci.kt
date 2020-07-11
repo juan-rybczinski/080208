f9dc29ea98678ad8efbb95f39447e541fdb7b402
juan-rybczinski <jhreplay.lee@gmail.com>
2020-07-11 09:23:27 +0900

    연습문제 4-3: 꼬리 재귀 버전의 피보나치 함수

--- parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise03/Fibonacci.kt
+++ parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise03/Fibonacci.kt
@@ -2,5 +2,12 @@ package com.fpinkotlin.recursion.exercise03
 
 import java.math.BigInteger
 
-
-fun fib(x: Int): BigInteger = TODO("fibonacci")
+fun fib(x: Int): BigInteger {
+    tailrec fun fib(val1: BigInteger, val2: BigInteger, x: BigInteger): BigInteger =
+            when (x) {
+                BigInteger.ZERO -> BigInteger.ONE
+                BigInteger.ONE -> val1 + val2
+                else -> fib(val2, val1 + val2, x - BigInteger.ONE)
+            }
+    return fib(BigInteger.ZERO, BigInteger.ONE, BigInteger.valueOf(x.toLong()))
+}
