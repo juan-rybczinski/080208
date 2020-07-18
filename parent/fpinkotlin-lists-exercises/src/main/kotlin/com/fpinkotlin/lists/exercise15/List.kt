2af266732c24d5c26882c313744d5ff4ed541ee7
juan-rybczinski <jhreplay.lee@gmail.com>
2020-07-18 10:21:00 +0900

    연습문제 5-15: 리스트의 리스트를 받아서 내포된 리스트에 들어 있는 모든 원소를 펼친 평평한 리스트를 반환하는 함수

--- parent/fpinkotlin-lists-exercises/src/main/kotlin/com/fpinkotlin/lists/exercise15/List.kt
+++ parent/fpinkotlin-lists-exercises/src/main/kotlin/com/fpinkotlin/lists/exercise15/List.kt
@@ -94,7 +94,7 @@ sealed class List<out A> {
                     is Cons -> coFoldRight(f(list.head)(acc), list.tail, identity, f)
                 }
 
-        fun <A> flatten(list: List<List<A>>): List<A> = TODO("flatten")
+        fun <A> flatten(list: List<List<A>>): List<A> = list.foldRight(invoke()) { x -> x::concat }
 
         operator fun <A> invoke(vararg az: A): List<A> =
                 az.foldRight(Nil) { a: A, list: List<A> -> Cons(a, list) }
